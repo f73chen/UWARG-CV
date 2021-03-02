57c02c1576244822154f0c24a224528504b435d3
rayjinghaolei <rayjinghaolei@gmail.com>
2021-03-02 17:46:46 -0500

    outputs the bounding box coordinates

--- boxDetection/utils/plots.py
+++ boxDetection/utils/plots.py
@@ -59,6 +59,10 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    print("top left x: " + str(int(x[0])))
+    print("top left y: " + str(int(x[1])))
+    print("bot right x: " + str(int(x[2])))
+    print("bot right y: " + str(int(x[3])))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
